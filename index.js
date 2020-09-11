74c5ef1c907f81ccb751f97e089cf74257447e4f
voidstarr <voidstarr@users.noreply.github.com>
2020-09-11 02:07:22 +0000

    update automatic message sending logic

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const client = new Discord.Client();
 var latestZoomInfo = null;
 
 function checkZoom() {
+    var lastLatestZoomInfo = latestZoomInfo;
+
     getTodaysZoomInfo(config.username, config.password).then((zoomInfo) => {
-        if(zoomInfo.success) {
-            latestZoomInfo = zoomInfo;
-        } else {
-            latestZoomInfo = null;
-        }
+        latestZoomInfo = zoomInfo.success ? zoomInfo : null;
+    }).catch((reason) => {
+        console.log(reason);
+        latestZoomInfo = null;
     });
+
+    if(lastLatestZoomInfo === null && latestZoomInfo !== null){
+        client.channels.fetch(config.updateChannel).then((channel) => {
+            sendCurrentZoomInfo(channel);
+        });
+    }
 }
 
 function sendCurrentZoomInfo(channel) {
@@ -24,17 +31,10 @@ function sendCurrentZoomInfo(channel) {
         channel.send("No zoom information. Either there is no class today, or the bot doesn't have the info yet.\nIn any case, check https://www.cpp.edu/~dagershman/cs2600-001");
 }
 
-function cronCheck() {
-    checkZoom();
-    client.channels.fetch(config.updateChannel).then((channel) => {
-        sendCurrentZoomInfo(channel);
-    });
-}
-
 client.on("ready", () => {
     console.log("connected");
     checkZoom();
-    new CronJob("0 */6 * * *", cronCheck).start();
+    new CronJob("0 */6 * * *", checkZoom).start();
 });
 
 client.on("message", msg => {
@@ -42,7 +42,6 @@ client.on("message", msg => {
         sendCurrentZoomInfo(msg.channel);
     } else if (msg.content === "~!forceUpdate" && config.authorizedUsers.includes(msg.author.id)) {
         checkZoom();
-        sendCurrentZoomInfo(msg.channel);
     }
 });
 
